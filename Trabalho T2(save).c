c86834e5f5639463c8cdcaccc7fb0fc6bbe0838f
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-26 02:31:25 -0300

    Backup Commit
    Ainda preciso corrigir, ta horrível.

--- Trabalho T2(save).c
+++ Trabalho T2(save).c
@@ -51,7 +51,7 @@ void PrimosIntervalo(int min, int max)
     {
         if (verifprimo(i) == true)
         {
-            printf("\t %d", i);
+            printf("  %d", i);
         }
     }
     printf("\n");
@@ -115,29 +115,26 @@ int main()
     
     int qtdprimos[numint], menorqtd; //numero do intervalo 1;
     
-    i=0, j=0;
+    for(j=0; j<=numint;j++)
+    {    
+        qtdprimos[j] = contaprimos(v[i], v[i+1]);
+        i += 2;
+    }
+    //procura o intervalo com menos primos.
+    
+    menorqtd = qtdprimos[j], j=0, i=0, contador=0;
     for(j=0; j<=numint; j++)
     {
-        qtdprimos[j] = contaprimos(v[i], v[i+1]);
-        if(qtdprimos[j+1] < qtdprimos[j] )
+        if(qtdprimos[j] < qtdprimos[j+1])
         {
-            menorqtd = j+1;
-        }else
-        {
-            menorqtd = j;
+            menorqtd = qtdprimos[j];
+            contador = j;
         }
-    i+=2;
+        i+=2;
     }
 
-    
     //informa qual tem menos primos
-    for(j == 0; j < numint; j++)
-    {
-            i=
-            printf("\nO intervalo com menor quantidade de primos é o intervalo de %d até %d,"
-                   "ele possui %d primos", v[i], v[i+1], contaprimos(v[menorqtd-1], v[menorqtd]));
-    }
-   
+    printf("\nA menor quantidade de primos é %d e está no intervalo %d (%d até %d)", menorqtd, contador+1, v[contador], v[+1]);
    
     //Pega os intervalos e informa quantos primos há em cada.
     i = 0, j=0; 
@@ -152,16 +149,42 @@ int main()
 }
 
 /*
-intervalo x = i+1;
-max e min desse intervalo: v[i] e v[i+1]
+criar um vinculo entre o numero do intervalo, sua correspondencia(min e max) e quantos primos ele tem.
 
+verifprimo() -> verifica se o número é primo e retorna true ou false de acordo com a resposta
+contaprimos() -> Conta a quantidade de primos em um dado intervalo cedido pelos nos parâmetros.
+PrimosIntervalo() -> Imprime os primos que estão em um dado intervalo cedido nos parâmetros.
+
+-pegar quantidade de intervalos.
+int numint = (informado pelo usuario)
+
+-Pegar o vetor de intervalos v[] que o usuário informa. em que intervalo[i] é o mínimo e intervalo[i+1] é o máximo para todo intervalo i+1.
+Para isso, declara-se o vetor como 
+"int v[(numero de intervalos) *2] para que haja capacidade de armazenar todos os minimos e máximos.
+
+-verifica a quantidade de primos que há em cada intervalo.
 
 j=0, i=0;
-for(j=0; j<=numint; j++)
-{
-    contaprimos(v[i], v[i+1]) = qtdprimos[j];
-  
+for(j=0; j<=numint;j++)
+{    
+    int qtdprimos[j] = contaprimos(intervalo[i], intervalo[i+1]);
+    i += 2;
 }
 
-*/
+nesse caso, o vetor qtdprimos[i] corresponde ao intervalo i+1 (qtdprimos[0] é a quantidade de primos no 
+intervalo 1.)
+qtdprimos j = intervalo[i+1] e intervalo[i+2] 
+qtd primos j+1 = intervalo[i+3] e intervalor [i+4];
 
+-procura o intervalo com  menor numero de primos.
+
+j=0, i=0 
+int menor;
+for(j=0; j<= numint;j++)
+{
+    if(qtdprimos[i+1]< qtdprimos[i])
+    {
+        menor = i+1 ;       
+    }
+}
+*/
