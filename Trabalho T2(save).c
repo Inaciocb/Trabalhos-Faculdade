037f9653644130126e9521351b50d5f4fda1ff41
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-10-26 02:38:07 -0300

    Update Trabalho T2(save).c

--- Trabalho T2(save).c
+++ Trabalho T2(save).c
@@ -1,24 +1,3 @@
-/*
-Faça um programa que pede ao usuário o tamanho e o número de intervalos a considerar e imprime o 
-intervalo que contém menos primos e o número de primos nesse intervalo. Deve-se usar as funções 
-acima que consederar pertinentes. Esse é o T2. Por exemplo, se a entrada for 10 3, a saída deve 
-dizer que é o intervalo 21-30, com 2 primos. Os primeiros 3 intervalos com 10 números têm os seguintes
- primos:
-*/
-/*
-[X]Passo 1. pegar o n de intervalos
-
-
-[X]Passo 2. criar vetor que armazene 2 numeros para cada intervalo.
-
-
-[X]Passo 3. pegar os valores de min e max e
-atribuir aos intervalos.
-
-Passo 4. Imprimir qual intervalo tem menos primos.
-*/
-
-
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -147,44 +126,3 @@ int main()
   
 	return 0;
 }
-
-/*
-criar um vinculo entre o numero do intervalo, sua correspondencia(min e max) e quantos primos ele tem.
-
-verifprimo() -> verifica se o número é primo e retorna true ou false de acordo com a resposta
-contaprimos() -> Conta a quantidade de primos em um dado intervalo cedido pelos nos parâmetros.
-PrimosIntervalo() -> Imprime os primos que estão em um dado intervalo cedido nos parâmetros.
-
--pegar quantidade de intervalos.
-int numint = (informado pelo usuario)
-
--Pegar o vetor de intervalos v[] que o usuário informa. em que intervalo[i] é o mínimo e intervalo[i+1] é o máximo para todo intervalo i+1.
-Para isso, declara-se o vetor como 
-"int v[(numero de intervalos) *2] para que haja capacidade de armazenar todos os minimos e máximos.
-
--verifica a quantidade de primos que há em cada intervalo.
-
-j=0, i=0;
-for(j=0; j<=numint;j++)
-{    
-	int qtdprimos[j] = contaprimos(intervalo[i], intervalo[i+1]);
-	i += 2;
-}
-
-nesse caso, o vetor qtdprimos[i] corresponde ao intervalo i+1 (qtdprimos[0] é a quantidade de primos no 
-intervalo 1.)
-qtdprimos j = intervalo[i+1] e intervalo[i+2] 
-qtd primos j+1 = intervalo[i+3] e intervalor [i+4];
-
--procura o intervalo com  menor numero de primos.
-
-j=0, i=0 
-int menor;
-for(j=0; j<= numint;j++)
-{
-	if(qtdprimos[i+1]< qtdprimos[i])
-	{
-		menor = i+1 ;       
-	}
-}
-*/
