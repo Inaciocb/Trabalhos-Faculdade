a3f298b0f3fbfad7046476161733c7f0fea2cb46
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-11-05 18:44:28 -0300

    Update Trabalho T3.c

--- LAB1/Trabalho T3.c
+++ LAB1/Trabalho T3.c
@@ -93,7 +93,6 @@ double ExpoenteFracionario(double base, double valor)
     {
         i = i/10;
     }  
-    
     // retorna o valor base^(j/i), por meio da aplicação das propriedades dos logaritmos.
     return XnaY(i, (j/i)*(log(base)/log(i)));
 }
