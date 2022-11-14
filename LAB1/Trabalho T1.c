15854580bdeb978aea340a3031df8d95d9c3e516
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-11-14 20:00:24 -0300

    Update Trabalho T1.c

--- LAB1/Trabalho T1.c
+++ LAB1/Trabalho T1.c
@@ -14,20 +14,20 @@ void frescurinha()
 
 int main ()
 {
-  setlocale(LC_ALL, "portuguese");
+    setlocale(LC_ALL, "portuguese");
   
-  int chute, entrada, entrada1, i=0, max=0, min=0;
+    int chute, entrada, entrada1, i=0, max=0, min=0;
   
-  srand (time (0));
+    srand (time (0));
   
-  printf("O jogo terá qual valor máximo?\nDigite: ");
-  scanf("%d", &max);
+    printf("O jogo terá qual valor máximo?\nDigite: ");
+    scanf("%d", &max);
  
   
-  printf("\tInstruções:\nPense em um valor entre 0 e %d.\n\nEscreva ' 0 ' caso o chute do programa esteja certo.\n" 
+    printf("\tInstruções:\nPense em um valor entre 0 e %d.\n\nEscreva ' 0 ' caso o chute do programa esteja certo.\n" 
            "Escreva ' 1 ' caso o chute esteja abaixo do valor pensado. \nEscreva ' 2 ' caso o chute esteja acima.\n\t\n"
            "Para começar, qualquer número: ", max);
-  scanf("%d", &entrada1);
+    scanf("%d", &entrada1);
     
     do
     {
@@ -61,7 +61,7 @@ int main ()
        
     }while(entrada!=0);
       
-  printf("\nO programa acertou seu número em %d tentativas!", i);
+    printf("\nO programa acertou seu número em %d tentativas!", i);
   
   return 0;
 }
