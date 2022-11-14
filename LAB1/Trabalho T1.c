500fd54b828ffdef9abe890bccfc8f34a191cf5c
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-11-14 19:58:15 -0300

    ajeitando

--- LAB1/Trabalho T1.c
+++ LAB1/Trabalho T1.c
@@ -7,6 +7,11 @@ Turma: SI1 2022-2
 #include <stdlib.h>
 #include <locale.h>
 
+void frescurinha()
+{
+    printf("\n[--============================================--]\n");
+}
+
 int main ()
 {
   setlocale(LC_ALL, "portuguese");
@@ -17,35 +22,44 @@ int main ()
   
   printf("O jogo terá qual valor máximo?\nDigite: ");
   scanf("%d", &max);
-  
-  chute = (rand () % (max+1)) ;
+ 
   
   printf("\tInstruções:\nPense em um valor entre 0 e %d.\n\nEscreva ' 0 ' caso o chute do programa esteja certo.\n" 
            "Escreva ' 1 ' caso o chute esteja abaixo do valor pensado. \nEscreva ' 2 ' caso o chute esteja acima.\n\t\n"
-           "Para começar, digite 0: ", max);
+           "Para começar, qualquer número: ", max);
   scanf("%d", &entrada1);
     
     do
     {
         i++;
+        chute = (rand () % (max+1-min))+min;
         printf("\nChute do programa: %d\n", chute);
         printf("\nEstá certo?\n0 = Sim\n1 = Está abaixo.\n2 = Está acima.\n Digite: ");
         scanf("%d", &entrada);
         
-        if(entrada == 2)
+        switch(entrada)
         {
-            max = chute - 1;
-            chute=(rand() % (max-min))+min;
-        
-        }
-        
-        else if(entrada == 1)
-        {
-            min = chute + 1;
-            chute = (rand() % (max-min))+min;
+            case 1:
+                min = chute+1;
+                break;
+            case 2:
+                max = chute - 1;
+                break;
+            case 0:
+                frescurinha();
+                printf("  O programa acertou seu número em %d tentativas!", i);
+                frescurinha();
+                break;
+            default:
+                printf("\n\n\n\n");
+                frescurinha();
+                printf("\t    Digite um valor válido.");
+                frescurinha();
+                printf("\n");
+                break;
         }
        
-    }while(entrada1==0 && entrada!=0);
+    }while(entrada!=0);
       
   printf("\nO programa acertou seu número em %d tentativas!", i);
   
