8fc74b8c5102963c58be3cdd5e2801106ff0e5cf
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-12-20 23:17:51 -0300

    Update T5.c

--- LAB1/T5.c
+++ LAB1/T5.c
@@ -2,54 +2,78 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <stdbool.h>
 
-void sorteia_cores(int tam, char cores[tam])
+bool verifica_repeticao(char coresSorteadas[4])
 {
-char coresPossiveis[7] = {'Y', 'O', 'R', 'G', 'B', 'P', 'V'};
-char coresSorteadas[4] = {'\0'}; 
-int numCoresSorteadas = 0; 
-
-
-srand(time(0));
-
-for (int i = 0; i < tam; i++)
-{
-    char cor = coresPossiveis[rand() % 7];
-
-   
-    while (numCoresSorteadas > 0 && coresSorteadas[numCoresSorteadas - 1] == cor)
+    for(int i = 1; i < 4; i++)
     {
-        cor = coresPossiveis[rand() % 7];
+        if(coresSorteadas[i] != coresSorteadas[0])
+            return false;
     }
-
-    coresSorteadas[numCoresSorteadas] = cor; 
-    cores[i] = cor; 
-    numCoresSorteadas++;
+    return true;
 }
+
+void sorteia_cores(char cores[4])
+{
+   char coresPossiveis[7] = {'Y', 'O', 'R', 'G', 'B', 'P', 'V'};
+   char coresSorteadas[4] = {'\0'}; 
+   int numCoresSorteadas = 0; 
+
+   srand(time(0));
+   do
+   {
+       numCoresSorteadas = 0;
+       for (int i = 0; i < 4; i++)
+       {
+           char cor = coresPossiveis[rand() % 7];
+
+           while (numCoresSorteadas > 0 && coresSorteadas[numCoresSorteadas - 1] == cor)
+           {
+               cor = coresPossiveis[rand() % 7];
+           }
+           coresSorteadas[numCoresSorteadas] = cor; 
+           cores[i] = cor; 
+           numCoresSorteadas++;
+       }
+   } while (verifica_repeticao(coresSorteadas) == true);
+}
+
+void jogadas(char coresSorteadas[4], char jogada[4])
+{
+    int contador = 0, numJogadas = 0;
+
+    printf("%s\n", coresSorteadas);
+
+    do
+    {
+        printf("Digite sua jogada: ");
+        scanf("%s", jogada);
+        for(int i = 0; i < 4; i++)
+        {
+            if(jogada[i] == coresSorteadas[i])
+                contador++;
+        }
+        if(contador < 4)
+        {
+            printf("Voce acertou uma sequencia de %d letras\n", contador);
+            contador = 0;
+        }
+        numJogadas++;
+    
+    } while(contador != 4 && numJogadas < 10);
+    
+    if (contador == 4)
+        printf("Parabens, voce acertou a sequência!\n");
+    else
+        printf("Infelizmente, voce esgotou suas tentativas sem acertar a sequência.\n");
 }
 
 int main()
 {
-char coresSorteadas[4], jogada[4];
-int contador;
+    char coresSorteadas[4], jogada[4];
+    sorteia_cores(coresSorteadas);
+    jogadas(coresSorteadas, jogada);
 
-sorteia_cores(4, coresSorteadas);
-
-printf("%s\n", coresSorteadas);
-do
-{
-    printf("Digite sua jogada: ");
-    scanf("%s", jogada);
-    contador = 0;
-    for(int i = 0; i < 4; i++)
-    {
-        if(jogada[i] == coresSorteadas[i])
-            contador++;
-    }
-    printf("Voce acertou uma sequencia de %d letras\n", contador);
-} while(contador != 4);
-
-printf("Parabens, voce acertou a sequência!\n");
-
-return 0;
+    return 0;
 }
