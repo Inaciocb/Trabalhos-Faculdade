3ad72cfb11ae645333b13e9b508542e504d58078
Inácio <67322986+Inaciocb@users.noreply.github.com>
2022-12-26 13:29:20 -0300

    Update T5.c

--- LAB1/T5.c
+++ LAB1/T5.c
@@ -4,6 +4,23 @@
 #include <string.h>
 #include <stdbool.h>
 
+int i = 0;
+
+void cor_texto(int vermelho, int verde, int azul)
+{
+    printf("\e[38;2;%d;%d;%dm", vermelho, verde, azul);
+}
+
+void cor_fundo(int vermelho, int verde, int azul)
+{
+    printf("\e[48;2;%d;%d;%dm", vermelho, verde, azul);
+}
+
+void cor_normal(void)
+{
+    printf("\e[m");
+}
+
 bool verifica_repeticao(char coresSorteadas[4])
 {
     for(int i = 1; i < 4; i++)
@@ -16,10 +33,8 @@ bool verifica_repeticao(char coresSorteadas[4])
 
 void sorteia_cores(char cores[4])
 {
-   char coresPossiveis[7] = {'Y', 'O', 'R', 'G', 'B', 'P', 'V'};
-   char coresSorteadas[4] = {'\0'}; 
+   char coresPossiveis[7] = {'Y', 'O', 'R', 'G', 'B', 'P', 'V'}, coresSorteadas[4] = {'\0'}; ;
    int numCoresSorteadas = 0; 
-
    srand(time(0));
    do
    {
@@ -28,7 +43,7 @@ void sorteia_cores(char cores[4])
        {
            char cor = coresPossiveis[rand() % 7];
 
-           while (numCoresSorteadas > 0 && coresSorteadas[numCoresSorteadas - 1] == cor)
+           while(numCoresSorteadas > 0 && coresSorteadas[numCoresSorteadas - 1] == cor)
            {
                cor = coresPossiveis[rand() % 7];
            }
@@ -39,39 +54,77 @@ void sorteia_cores(char cores[4])
    }while (verifica_repeticao(coresSorteadas) == true);
 }
 
-void jogadas(char coresSorteadas[4], char jogada[4])
+char converte_minusculas(char letra)
 {
-    int contador = 0, numJogadas = 0;
-
-    printf("%s\n", coresSorteadas);
-
-    do
+    switch(letra)
     {
-        printf("Digite sua jogada: ");
-        scanf("%s", jogada);
-        for(int i = 0; i < 4; i++)
-        {
-            if(jogada[i] == coresSorteadas[i])
-                contador++;
-        }
-        if(contador < 4)
-        {
+        case 'y':
+            return 'Y';
+        case 'o':
+            return 'O';
+        case 'r':
+            return 'R';
+        case 'g':
+            return 'G';
+        case 'b':
+            return 'B';
+        case 'p':
+            return 'P';
+        case 'v':
+            return 'V';
+        default:
+            return letra;
+    }
+}
+
+void resultado(int contador, char jogada[4], char coresSorteadas[4])
+{
+    for(i  = 0; i < 4; i++)
+    {
+        jogada [i] = converte_minusculas(jogada[i]);
+    }
+    
+    for(int i = 0; i < 4; i++)
+    {
+        if(jogada[i] == coresSorteadas[i])
+        contador++;
+    }
+    if(contador < 4)
+    {
+        if (contador > 0)
             printf("\nVocê acertou uma sequência de %d letras\n", contador);
-            contador = 0;
-        }
-        numJogadas++;
-    
-    }while(contador != 4 && numJogadas < 10);
-    
-    if(contador == 4)
+        else
+            printf("Você não acertou nenhuma das cores na sequência :(\n");
+        contador = 0;
+    }
+    if (contador == 4)
         printf("Parabéns, você acertou a sequência!\n");
     else
         printf("Você esgotou suas tentativas sem acertar a sequência.\n");
+}    
+
+void faz_jogada(char jogada[4])
+{
+    printf("Digite sua jogada: ");
+    scanf("%s", jogada);
+}
+
+void jogadas(char coresSorteadas[4], char jogada[4])
+{
+    int contador = 0, numJogadas = 0;
+    printf("%s\n", coresSorteadas);
+    do
+    {
+        faz_jogada(jogada);
+        resultado(contador, jogada, coresSorteadas);
+        numJogadas++;
+    }while(contador != 4 && numJogadas < 10);
+    resultado(contador, jogada, coresSorteadas);
 }
 
 int main()
 {
-    char coresSorteadas[4], jogada[4];
+    char coresSorteadas[4], jogada[4]; 
     sorteia_cores(coresSorteadas);
     jogadas(coresSorteadas, jogada);
 
